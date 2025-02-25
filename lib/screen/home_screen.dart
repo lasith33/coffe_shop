1e060ac98dc302d8bc9013e313828eb6a980e006
lasith chamuditha <125144995+lasith33@users.noreply.github.com>
2025-02-25 20:47:37 +0530

    add notification icon and menu icon

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
+    super.initState();
+  }
+
+  _handleTabSelection(){
+    if(_tabController.indexIsChanging){
+      setState(() {
+
+      });
+    }
+  }
+
+  @override
+  void dispose(){
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: ListView(
+            children: [
+              Padding(
+                  padding:EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: (){},
+                      child: Icon(
+                        Icons.sort_rounded,
+                        color: Colors.white.withOpacity(0.5),
+                        size: 35,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: (){},
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.white.withOpacity(0.5),
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/welcome_screen.dart
+++ lib/screen/welcome_screen.dart
@@ -1,4 +1,6 @@
+import 'package:coffe_shop/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -12,7 +14,60 @@ class WelcomeScreen extends StatelessWidget {
           color: Colors.black,
           image: DecorationImage(
             image: AssetImage("images/bg.png"),
+            fit: BoxFit.cover,
+            opacity: 0.6,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Coffee Shop",
+              style: GoogleFonts.pacifico(
+              fontSize: 50,
+              color: Colors.white
+            ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Feeling Low? Take a sip of coffee",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500, 
+                  ),
+                  ),
+                  SizedBox(height: 80),
+
+                  Material(
+                    color: Color(0xFFE57734),
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(context,MaterialPageRoute(
+                            builder: (context)=>HomeScreen(),
+                        ),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 15,horizontal: 50),
+
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                          ),
+                          ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
         ),
       ),
     );
